89520f43526eecf8713afc0f0f8cf394f94bb250
Kyle Lobo <kylelobo11898@gmail.com>
2021-10-31 23:23:02 +0530

    remove redundant if condition

--- Electron/back.js
+++ Electron/back.js
@@ -3,13 +3,6 @@ const path = require('path');
 
 var back = document.getElementById('back');
 back.addEventListener('click', () => {
-    if (process.platform !== 'darwin') {
-        vid_url='main.html'
-        electron.remote.getCurrentWindow().loadURL(`file://${__dirname}/${vid_url}`);
-
-    }
-    else {
-        vid_url='main.html'
-        electron.remote.getCurrentWindow().loadURL(`file://${__dirname}/${vid_url}`);
-    }
+    vid_url='main.html'
+    electron.remote.getCurrentWindow().loadURL(`file://${__dirname}/${vid_url}`);
 })
